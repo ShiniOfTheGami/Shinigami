577e52619eefbf0c47664151c9a4a131c3c7c96e
Shiggysama <ctbuschmann@gmail.com>
2013-09-06 18:22:17 +0200

    Update Frenzy.js

--- Frenzy.js
+++ Frenzy.js
@@ -9,8 +9,8 @@ function addFrenzyClick() {
 	};
 	document.body.appendChild(FrenzyClick);
 	var MenuButton = document.createElement('div');
-    	MenuButton.id = 'MenuButton';
-    	MenuButton.innerHTML = '<p>Cheats</p>';
+    MenuButton.id = 'MenuButton';
+    MenuButton.innerHTML = '<p>Cheats</p>';
 	MenuButton.onclick = function(){
 		if(document.getElementById('Menu').style.display == 'block'){
 			document.getElementById('Menu').style.display = 'none';
@@ -23,22 +23,23 @@ function addFrenzyClick() {
 	var Menu = document.createElement('div');
 	Menu.id = 'Menu';
 	Menu.style.cssText = 'display:none';
-	Menu.innerHTML = '<button id="AutoClick" type="button" onclick="setInterval(function() {Game.ClickCookie();}, 1);">AutoClick</button>';
+	Menu.innerHTML = '<button id="AutoClick" type="button" onclick="setInterval(function() {Game.ClickCookie();}, 1);">AutoClick</button><button id="AutoGolden" type="button" onclick="setInterval(function() {if (Game.goldenCookie.life > 0) Game.goldenCookie.click();}, 100);">AutoGolden</button>';
 	document.getElementById('sectionMiddle').appendChild(Menu);
 }
 function styling(){
-	var FrenzyClickstyle = document.styleSheets[0];
-	FrenzyClickstyle.addRule('#FrenzyClick','width:300px;height:44px;background-color:white;z-index:1000;position:fixed;top:31px;right:18px;box-shadow:inset 0 0 5px #000;margin:0px;padding:0px;');
-	FrenzyClickstyle.addRule('#FrenzyClick:hover', 'cursor:pointer; font-size: 24px;');
-	FrenzyClickstyle.addRule('#FrenzyClick p', 'margin:0px;padding:0px;line-height:44px;color:#000;text-align:center;font-size: 20px;');
-	FrenzyClickstyle.addRule('#MenuButton','padding:14px 16px 10px 0px;top:0px;right:-16px;z-index:3000;');
-	//FrenzyClickstyle.addRule('#MenuButton p','margin:0px;padding:0px;line-height:32px;color:#000;text-align:center;font-size: 20px;box-shadow:inset 0 0 5px #000;');
-	FrenzyClickstyle.addRule('#Menu','height:96px;position:absolute;width:100%;top:0px;background-color:white;z-index:2000;box-shadow:inset 0 0 5px #000;overflow-x:hidden;');
-	FrenzyClickstyle.addRule('#AutoClick','left:1%;top:1%');
+var FrenzyClickstyle = document.styleSheets[0];
+FrenzyClickstyle.addRule('#FrenzyClick','width:300px;height:44px;background-color:white;z-index:1000;position:fixed;top:31px;right:18px;box-shadow:inset 0 0 5px #000;margin:0px;padding:0px;');
+FrenzyClickstyle.addRule('#FrenzyClick:hover', 'cursor:pointer; font-size: 24px;');
+FrenzyClickstyle.addRule('#FrenzyClick p', 'margin:0px;padding:0px;line-height:44px;color:#000;text-align:center;font-size: 20px;');
+FrenzyClickstyle.addRule('#MenuButton','padding:14px 16px 10px 0px;top:0px;right:-16px;z-index:3000;');
+//FrenzyClickstyle.addRule('#MenuButton p','margin:0px;padding:0px;line-height:32px;color:#000;text-align:center;font-size: 20px;box-shadow:inset 0 0 5px #000;');
+FrenzyClickstyle.addRule('#Menu','height:96px;position:absolute;width:100%;top:0px;background-color:white;z-index:2000;box-shadow:inset 0 0 5px #000;overflow-x:hidden;');
+FrenzyClickstyle.addRule('#AutoClick','height:86px;width:30%;margin-left:1%;margin-top:5px;');
+FrenzyClickstyle.addRule('#AutoGolden','height:86px;width:30%;margin-left:1%;margin-top:5px;');
 }
 
 Startup = setInterval(function() {
-	addFrenzyClick();
-	styling();
-	clearInterval(Startup);
+addFrenzyClick();
+styling();
+clearInterval(Startup);
 }, 10);
